39b73d2415606e066f82ac239f1f67f898f6e922
Alex Chan <everyothernamewastaken@gmail.com>
2017-08-18 19:15:50 +1000

    .

--- src/js/components/gamejoin.js
+++ src/js/components/gamejoin.js
@@ -1,7 +1,6 @@
 import React from "react"
 
-export default class Game extends React.Component{    
-
+export default class Game extends React.Component{
     render(){
   return (<h1>this da join game</h1>)
 
